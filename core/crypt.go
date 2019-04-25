2ba0ad891a63faeef36d21788f37f978f81be789
basebandit <8973567+basebandit@users.noreply.github.com>
2019-04-25 14:58:35 +0300

    Added Licence clause

--- core/crypt.go
+++ core/crypt.go
@@ -1,3 +1,9 @@
+// Copyright 2019 Basebandit. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+//Encrypt and Decrypt
+
 package crypt
 
 import (
