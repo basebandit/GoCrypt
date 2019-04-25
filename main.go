c74f8af3824adabb9cc789539c7fbd3db48c1f8f
basebandit <8973567+basebandit@users.noreply.github.com>
2019-04-25 10:25:35 +0300

    Refactored command line arguments

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"crypto/rand"
-	"crypto/sha256"
 	"flag"
 	"fmt"
 	"os"
@@ -10,21 +8,7 @@ import (
 	"github.com/basebandit/GoCrypt/core"
 )
 
-//Uninitialized KeyStore
-var ks *core.KeyStore
-
-var key []byte
-
 func main() {
-
-	//It is recommended that your salt be at least 8 bytes long
-	salt := make([]byte, len("53cr37_s@l7"))
-	_, err := rand.Read(salt)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
 
 	//CLI Commands
 	encryptCmd := flag.NewFlagSet("encrypt", flag.ExitOnError)
@@ -47,10 +31,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Switch on the subcommand
-	// Parse the flags for appropriate FlagSet
-	// FlagSet.Parse() requires a set of arguments to parse as input
-	// os.Args[2:] will be all arguments starting after the subcommand at os.Args[1]
 	switch os.Args[1] {
 	case "encrypt":
 		encryptCmd.Parse(os.Args[2:])
@@ -61,8 +41,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Check which subcommand was Parsed using the FlagSet.Parsed() function. Handle each case accordingly.
-	// FlagSet.Parse() will evaluate to false if no flags were parsed (i.e. the user did not provide any flags)
 	if encryptCmd.Parsed() {
 		var msg, pwd string
 		// Required Flags
@@ -78,22 +56,7 @@ func main() {
 		}
 		pwd = *encryptPwd
 
-		//First Step: Initialize a new KeyGenerator
-		kg := core.NewKeyGen([]byte(pwd), []byte(salt), 10000, 32, sha256.New)
-
-		//Second Step: Generate the key using the above KeyGenerator
-		key = kg.HashPassword()
-
-		if ks == nil {
-			//Third Step: Store the generated Key for future use to encrypt/decrypt
-			ks = core.NewKeyStore(key)
-		}
-		encryptor := core.Encryptor{Message: msg}
-		//Fourth Step: Use the key
-		ciphertext, err := encryptor.Encrypt(ks.Key)
-		if err != nil {
-			panic(err)
-		}
+		ciphertext := core.Encrypt(msg, pwd)
 
 		fmt.Printf("%s\n", ciphertext)
 	}
@@ -112,16 +75,9 @@ func main() {
 			os.Exit(1)
 		}
 		pwd = *decryptPwd
-		// Print
-		fmt.Printf("source: %s, password: %s\n", ciphertext, pwd)
-		fmt.Println("Ks", ks)
-		fmt.Println("Key", key)
+		plaintext := core.Decrypt(ciphertext, pwd)
+
+		fmt.Printf("%s\n", plaintext)
 	}
-	fmt.Println("Key", key)
-	// decryptor := core.Decryptor{Ciphertext: ciphertext}
-	// plaintext, err := decryptor.Decrypt(ks.Key)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Printf("%s\n", plaintext)
+
 }
